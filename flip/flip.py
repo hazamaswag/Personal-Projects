9dc3622ea950c535ac3366eae53a792ee144ea42
Hamza Sayyid <hamzasayyid@Hamzas-MacBook-Pro.local>
2019-06-15 19:07:17 -0700

    Flipped! (but only for square images)

--- flip/flip.py
+++ flip/flip.py
@@ -5,23 +5,8 @@ pix = im.load()
 x_dim, y_dim = im.size
 print("dimensions:", x_dim, y_dim)
 
-for x in range(x_dim ):
+for x in range(x_dim // 2):
     for y in range(y_dim):
-        print("(",x, ",", y,")", " ---->  (", pix[x,y], ")",sep="")
-        print("(",x, ",", y_dim - y - 1,")", " ---->  (", pix[x,y_dim - y - 1], ")",sep="")
-        pix[x,y] = pix[y,x]
-        print("-------------------------------------------------------")
-        print("(",x, ",", y,")", " ---->  (", pix[x,y], ")",sep="")
-        print("(",x, ",", y_dim - y - 1,")", " ---->  (", pix[x,y_dim - y - 1], ")",sep="")
-        print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+        pix[x,y], pix[x_dim - x - 1, y] = pix[x_dim - x - 1, y], pix[x,y]
 
 im.save('obama_flipped.png')
-# pix[x,y_dim - y - 1], pix[x,y] = pix[x,y], pix[x, y_dim - y - 1]
-# def flip(x):
-#     print("og:", x)
-#     y = []
-#     for i in x:
-#         y += [i[::-1]]
-#     print("new",  y)
-#
-# flip([[1,2,3],[4,5,6],[7,8,9]])
